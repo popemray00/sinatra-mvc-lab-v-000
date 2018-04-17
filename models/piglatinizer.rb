500e0a076ae7c5c10ab6125f27acef9964ee1e92
Michael Pope <popemray1986@gmail.com>
2018-04-17 03:12:19 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,22 @@
+class PigLatinizer
+
+  def to_pig_latin(sentence)
+    word = sentence.split(" ")
+    word.map! {|word| piglatinize(word)}.join(" ")
+  end
+
+
+  def piglatinize(word)
+    if word.include?(" ")
+       to_pig_latin(word)
+    else
+      first_split = word.split(/([aeiouAEIOU].*)/)[0]
+      second_split = word.split(/([aeiouAEIOU].*)/)[1]
+      if first_split == ""
+         second_split + "way"
+      else
+         second_split + first_split + "ay"
+      end
+    end
+  end
+end
