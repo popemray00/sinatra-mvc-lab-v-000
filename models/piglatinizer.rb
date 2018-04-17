66695bc8636d11158928ded10fc0201ec499fc34
Michael Pope <popemray1986@gmail.com>
2018-04-17 03:40:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,31 +1,33 @@
-def piglatinize(word)
-  vowels = ["a", "e", "i", "o", "u"]
-  consonants = ""
+class PigLatinizer
 
-  if word == "I" || word == "A"
-    word + "way"
+   def piglatinize(word)
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = ""
 
-  elsif vowels.include?(word[0].downcase)
-    word + "way"
+    if word == "I" || word == "A"
+      word + "way"
 
-  elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
-    word2 = word.split("")[3..-1].join
-    word2 + word[0] + word[1] + word[2] + "ay"
+    elsif vowels.include?(word[0].downcase)
+      word + "way"
 
-  elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
-    word2 = word.split("")[2..-1].join
-    word2 + word[0] + word[1] + "ay"
+    elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
+      word2 = word.split("")[3..-1].join
+      word2 + word[0] + word[1] + word[2] + "ay"
 
-    elsif
-    !vowels.include?(word[0])
-     word2 = word.split("")[1..-1].join
-     word2 + word[0] + "ay"
+    elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
+      word2 = word.split("")[2..-1].join
+      word2 + word[0] + word[1] + "ay"
 
+      elsif
+      !vowels.include?(word[0])
+       word2 = word.split("")[1..-1].join
+       word2 + word[0] + "ay"
+
+    end
+   end
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
   end
- end
-
-def to_pig_latin(string)
-  string.split.collect{|word| piglatinize(word)}.join(" ")
-end
 
 end
