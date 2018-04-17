8ee9bd7e164c2a3708bf90e003d8ccbe4a634eef
Michael Pope <popemray1986@gmail.com>
2018-04-17 03:15:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :user_input
   end
@@ -11,5 +12,5 @@ class App < Sinatra::Base
 
     erb :results
   end
-  
+
 end
