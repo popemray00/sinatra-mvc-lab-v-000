8a08f8b0be7ea0734d733a3ea7362acdeef7fcc8
Michael Pope <popemray1986@gmail.com>
2018-04-17 03:54:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,29 @@
 class PigLatinizer
-  VOWELS = /a|e|i|o|u/i
-
-  def piglatinize(word)
-    if word[0] =~ VOWELS
-      word + 'way'
-    else
-      pieces = word.split(VOWELS)
-      first_consonant = pieces.first
-      word[first_consonant.length..-1] + first_consonant + 'ay'
-    end
+   attr_reader :text
+  def initialize
+     @text = text
   end
 
-   def to_pig_latin(str)
-   str.split(" ").map do |word|
-     piglatinize(word)
-   end.join(" ")
- end
+  def piglatinize(text)
+    words = text.split
+    piglatin = []
+    vowels = ['a', 'e', 'i','o', 'u']
+    words.each do |word|
+      pig = word.split('')
+      i_first_vowel = pig.find_index{|letter| vowels.include?(letter.downcase)}
+      if i_first_vowel == 0
+        pig << "way"
+        piglatin << pig.join
+      else
+        pig.rotate!(i_first_vowel)
+        pig << "ay"
+        piglatin << pig.join
+      end
+    end
+    piglatin.join(' ')
+  end
 
-
+  def to_pig_latin(sentence)
+    piglatinize(sentence)
+  end
 end
